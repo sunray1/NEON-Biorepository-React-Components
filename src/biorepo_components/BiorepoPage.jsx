6c7aca87223b1851cc392cd81f02978705bfdebf
Chandra Earl <sunray1@bellsouth.net>
2025-01-14 13:00:01 -0500

    Load symbini config and use

--- src/biorepo_components/BiorepoPage.jsx
+++ src/biorepo_components/BiorepoPage.jsx
@@ -6,9 +6,10 @@ import React, {
 import NeonPage from '../lib_components/components/NeonPage/NeonPage';
 
 export default function BiorepoPage() {
+  const [config, setConfig] = useState(null);
+
   const getDefaultSidebarLinks = () => {
-    if (window.location.pathname === '/neon/portal/collections/datasets/publiclist.php') {
-    // if (window.location.pathname === '/neon/collections/datasets/publiclist.php') {
+    if (window.location.pathname.endsWith('/publiclist.php')) {
       return [
         {
           name: 'Papers & Publications',
@@ -26,19 +27,55 @@ export default function BiorepoPage() {
 
   // Get breadcrumbs and sidebar from json based on pathname
   const [breadcrumbs, setBreadcrumbs] = useState([]);
-  const [sidebarLinks, setSidebarLinks] = useState(getDefaultSidebarLinks());
+  const [sidebarLinks, setSidebarLinks] = useState([]);
   const [sidebarTitle, setSidebarTitle] = useState('');
 
   useEffect(() => {
+    // Fetch configuration from getSymbiniConfig.php
+    const fetchConfig = async () => {
+      const basePath = window.location.origin;
+      const currentPath = window.location.pathname.split('/').slice(0, -1).join('/');
+      const pathSegments = currentPath.split('/').filter(Boolean);
+      let clientRootPath = '/';
+      for (let i = 1; i <= pathSegments.length; i += 1) {
+        const candidatePath = `/${pathSegments.slice(0, i).join('/')}/neon-react`;
+        // Check if the resource exists (use HEAD request to test for existence)
+        const xhr = new XMLHttpRequest();
+        xhr.open('HEAD', `${basePath}${candidatePath}`, false);
+        xhr.send();
+        if (xhr.status === 200) {
+          clientRootPath = `/${pathSegments.slice(0, i).join('/')}`;
+          break;
+        }
+      }
+      const fetchUrl = `${basePath}${clientRootPath}/neon-react/biorepo_lib/getSymbiniConfig.php`;
+      console.log(fetchUrl);
+      try {
+        const response = await fetch(fetchUrl);
+        if (!response.ok) {
+          throw new Error('Failed to fetch configuration');
+        }
+        const data = await response.json();
+        setConfig(data);
+      } catch (error) {
+        console.error('Error fetching configuration:', error);
+      }
+    };
+
+    fetchConfig();
+  }, []);
+
+  useEffect(() => {
+    if (!config) return;
+    setSidebarLinks(getDefaultSidebarLinks());
     const fetchBreadcrumbs = async () => {
       try {
-        const response = await fetch('/neon/portal/neon-react/biorepo_lib/breadcrumbs.json');
-        // const response = await fetch('/neon/neon-react/biorepo_lib/breadcrumbs.json');
+        const response = await fetch(`${config.CLIENT_ROOT}/neon-react/biorepo_lib/breadcrumbs.json`);
         if (!response.ok) {
           throw new Error('Failed to fetch breadcrumbs');
         }
         const data = await response.json();
-        const { pathname } = window.location;
+        const pathname = window.location.pathname.slice(config.CLIENT_ROOT.length);
         const breadcrumbData = data[pathname] || [{ name: 'Home', href: '/' }];
         setBreadcrumbs(breadcrumbData);
 
@@ -53,13 +90,12 @@ export default function BiorepoPage() {
 
     const fetchSidebarLinks = async () => {
       try {
-        const response = await fetch('/neon/portal/neon-react/biorepo_lib/sidebar.json');
-        // const response = await fetch('/neon/neon-react/biorepo_lib/sidebar.json');
+        const response = await fetch(`${config.CLIENT_ROOT}/neon-react/biorepo_lib/sidebar.json`);
         if (!response.ok) {
           throw new Error('Failed to fetch sidebar links');
         }
         const data = await response.json();
-        const { pathname } = window.location;
+        const pathname = window.location;
 
         const sidebarData = data[pathname] || data.default;
         setSidebarLinks(sidebarData.links);
@@ -71,7 +107,7 @@ export default function BiorepoPage() {
 
     fetchBreadcrumbs();
     fetchSidebarLinks();
-  }, []);
+  }, [config]);
 
   return (
     <NeonPage
